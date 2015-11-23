47e2a89295125f4143f062afb6f9df3390c5a01e
en213210023 <elvis.nwokocha@students.stamford.edu>
2015-11-23 12:47:13 +0700

    week2

--- Week2/js/add-content.js
+++ Week2/js/add-content.js
@@ -1,3 +1,11 @@
-body {
-	background-color: grey;
-}
+var price;
+var quantity;
+var total;
+
+price = 5;
+quantity = 14;
+total = price * quantity;
+
+//document.write("Total is " + total);
+var totaPriceEle = document.getElementById("totalPrice");
+totalPriceEle.textContent = "$" + total;
