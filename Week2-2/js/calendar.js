6a94823b08c7e95eecda378650e1167ca9a2eb47
en213210023 <elvis.nwokocha@students.stamford.edu>
2015-12-14 18:19:42 +0700

    Hotel calendar renewed

--- Week2-2/js/calendar.js
+++ Week2-2/js/calendar.js
@@ -1,11 +1,17 @@
 // display the calendar here
 var hotelTable = document.getElementById("hotelCalendarTable");
 var defaultData = hotelTable.innerHTML;
+var days = 2;
 var stringHTML = "";
-//stringHTML = "<tr><td>1</td><td>2</td></tr>";
+stringHTML += "<tr><td>1</td>";
+// using while loop to input calendar days  from 1 - 31 days//
+while(days <= 31){
+	if(days % 7 == 1){
+		stringHTML += "</tr><br><tr><td>"+ days +"</td>"; // breaks days
+	} else { 
+		stringHTML += "<td>"+days+"</td>";
+	}
+	days++; // increamenting //
+}
+stringHTML += "</tr>"; // End of table//
 hotelTable.innerHTML += stringHTML;
-
-//insert values in cells
-for (var x = 1; x <= 31; x++) {
-        document.getElementById("cell" + x).innerHTML = x ;
-    }
